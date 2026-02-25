7ff4d1e05f00952a68a8b28f157f36a2c0dcfb55
saksham thapar <sakshamthapar22@gmail.com>
2026-02-25 09:24:16 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        char[] charArray = str.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            if (charArray[left] != charArray[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
